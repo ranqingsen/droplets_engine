aa56fac74db77866bfea08f0f28d334bbc145e96
jackran <ranqingsen@xindong.com>
2019-05-28 21:17:50 +0800

    Add demo setup and generate_project_files cmd files

---
+++ SetUp.bat
@@ -0,0 +1,6 @@
+@if not exist %~dp0\Engine\Externals mkdir %~dp0\Engine\Externals
+
+git clone https://github.com/ranqingsen/engine_external_library.git %~dp0\Engine\Externals\clang
+SetX PATH "%~dp0\Engine\Externals\clang\bin;%PATH%"
+
+
